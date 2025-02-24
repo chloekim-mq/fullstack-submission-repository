07fb027eb23b0a8a2e61dd0ae7700351aa8ae62e
Chloe Kim <chloeivykim@gmail.com>
2025-02-25 00:26:01 +1100

    update 2.13

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from 'react'
-import Filter from './Components/Filter'
-import PersonForm from './Components/PersonForm'
-import Persons from './Components/Persons'
+import { useState, useEffect } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+import axios from 'axios'
+import personService from './services/persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+  useEffect(() => {
+    personService
+    .getAll()
+    .then(initialPersons => {
+        setPersons(initialPersons)
+    })
+}
+, [])
 
   const addPerson = (event) => {
       event.preventDefault()
@@ -26,9 +31,20 @@ const App = () => {
           name: newName,
           number: newNumber
       }
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+
+      personService
+      .create(personObject)
+      .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName('')
+          setNewNumber('')
+      })
+
+      axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response => {
+          console.log(response)
+      })
   }
 
   const handleNameChange = (event) => {
